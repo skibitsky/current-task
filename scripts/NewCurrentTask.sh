f9c95c01182288ed0c342f35c0e631efe79ac66e
skibitsky <15127974+skibitsky@users.noreply.github.com>
2019-09-14 15:24:46 +0300

    Changed the way osascript receives a script path

--- scripts/NewCurrentTask.sh
+++ scripts/NewCurrentTask.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 
+DIR=`dirname "$0"`
 TASK=$1
 PROJECT=$2
 
 echo $TASK > $HOME/Library/Mobile\ Documents/iCloud~is~workflow~my~workflows/Documents/Current\ Task/ct.txt
 open focus://focus?minutes=25
 
-osascript RunThingsTask.applescript $TASK $PROJECT
+osascript $DIR/RunThingsTask.applescript $TASK $PROJECT
